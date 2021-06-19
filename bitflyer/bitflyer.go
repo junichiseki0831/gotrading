9c92f4a5092975b6b2972e88b0a1e7899324dcf3
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-06-19 22:22:57 +0900

    Add GetRealTimeTicker method

--- bitflyer/bitflyer.go
+++ bitflyer/bitflyer.go
@@ -6,12 +6,15 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strconv"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 const baseURL = "https://api.bitflyer.com/v1/"
@@ -139,7 +142,8 @@ func (t *Ticker) GetMidPrice() float64 {
 
 //リアルタイムの取得
 func (t *Ticker) DateTime() time.Time {
-	t.Timestamp = "2018-09-27T02:01:59.60052847"
+	//t.Timestamp = "2018-09-27T02:01:59.60052847"
+	//t.Timestamp = "2018-06-26T10:16:41.704"
 	dateTime, err := time.Parse(time.RFC3339, t.Timestamp)
 	if err != nil {
 		log.Printf("action=DateTime, err=%s", err.Error())
@@ -165,4 +169,108 @@ func (api *APIClient) GetTicker(productCode string) (*Ticker, error) {
 		return nil, err
 	}
 	return &ticker, nil
+}
+
+// Pubnub service is scheduled to stop on 1st Dec, 2018
+/*
+func (api *APIClient) GetRealTimeTicker(symbol string, ch chan<- Ticker) {
+	pubnub := messaging.NewPubnub(
+		"", "sub-c-52a9ab50-291b-11e5-baaa-0619f8945a4f",
+		"", "", false, "", nil)
+
+	channel := fmt.Sprintf("lightning_ticker_%s", symbol)
+	sucCha := make(chan []byte)
+	errCha := make(chan []byte)
+
+	// [[{"best_ask":6206.99,"best_ask_size":1.24,"best_bid":6164,"best_bid_size":0.3,"ltp":6184.1,"product_code":"BTC_USD","tick_id":33839,"timestamp":"2018-10-12T03:01:53.8597609Z","total_ask_depth":228.3295673,"total_bid_depth":15.3916763,"volume":37.29123857,"volume_by_product":37.29123857}], "15393133139745912", "lightning_ticker_BTC_USD"]
+	go pubnub.Subscribe(channel, "", sucCha, false, errCha)
+
+	OUTER:
+		for {
+			select {
+			case res := <-sucCha:
+				var tickerList []interface{}
+				if err := json.Unmarshal(res, &tickerList); err != nil {
+					continue OUTER
+				}
+				var ticker Ticker
+				switch tic := tickerList[0].(type){
+				case []interface{}:
+					if len(tic)	 == 0 {
+						continue OUTER
+					}
+					marshaTic, err := json.Marshal(tic[0])
+					if err != nil {
+						continue OUTER
+					}
+					if err := json.Unmarshal(marshaTic, &ticker); err != nil {
+						continue OUTER
+					}
+					ch <- ticker
+				}
+
+			case err := <-errCha:
+				log.Printf("action=GetRealTimeTicker err=%s", err)
+			case <-messaging.SubscribeTimeout():
+				log.Printf("action=GetRealTimeTicker err=timeout")
+			}
+		}
+}
+*/
+
+type JsonRPC2 struct {
+	Version string      `json:"jsonrpc"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
+	Result  interface{} `json:"result,omitempty"`
+	Id      *int        `json:"id,omitempty"`
+}
+
+type SubscribeParams struct {
+	Channel string `json:"channel"`
+}
+
+func (api *APIClient) GetRealTimeTicker(symbol string, ch chan<- Ticker) {
+	u := url.URL{Scheme: "wss", Host: "ws.lightstream.bitflyer.com", Path: "/json-rpc"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	channel := fmt.Sprintf("lightning_ticker_%s", symbol)
+	if err := c.WriteJSON(&JsonRPC2{Version: "2.0", Method: "subscribe", Params: &SubscribeParams{channel}}); err != nil {
+		log.Fatal("subscribe:", err)
+		return
+	}
+
+OUTER:
+	for {
+		message := new(JsonRPC2)
+		if err := c.ReadJSON(message); err != nil {
+			log.Println("read:", err)
+			return
+		}
+
+		if message.Method == "channelMessage" {
+			switch v := message.Params.(type) {
+			case map[string]interface{}:
+				for key, binary := range v {
+					if key == "message" {
+						marshaTic, err := json.Marshal(binary)
+						if err != nil {
+							continue OUTER
+						}
+						var ticker Ticker
+						if err := json.Unmarshal(marshaTic, &ticker); err != nil {
+							continue OUTER
+						}
+						ch <- ticker
+					}
+				}
+			}
+		}
+	}
 }
